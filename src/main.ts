8960818e5ef6ffd332dc07a6bc43b9106d22cdb6
Stiratto <moranmoralesj4@gmail.com>
2024-09-01 12:12:05 -0500

    Modified listening port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  await app.listen(4000);
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
 }
 bootstrap();
